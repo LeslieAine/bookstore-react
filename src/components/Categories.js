654a5eebb331af6b4c2fa1cab7254c88d60b49fd
LeslieAine <aineleslie@gmail.com>
2023-03-09 09:44:48 +0300

    Add methods

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,20 @@
-const Categories = () => (
-  <div>
-    <button type="button">
-      Check status
-    </button>
-  </div>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import Button from './Button';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories.status);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>{status}</h1>
+      <Button
+        type="button"
+        title="Check status"
+        onClick={() => dispatch(checkStatus())}
+      />
+    </div>
+  );
+};
+
 export default Categories;
